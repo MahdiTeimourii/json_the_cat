9d9ea49d8304e5b90ddbb08727de5e73dd8dc431
Mahdi Teimouri <mahditei@mail.com>
2023-05-11 14:21:40 -0700

    sadasd

---
+++ breedFetcher.js
@@ -0,0 +1,23 @@
+const request = require('request');
+
+const fetchBreedDescription = (breed, callback) => {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+
+  request(url, (err, res, body) => {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback(`No description found for breed '${breed}'`, null);
+      return;
+    }
+
+    const description = data[0].description;
+    callback(null, description);
+  });
+};
+
+module.exports = fetchBreedDescription;
